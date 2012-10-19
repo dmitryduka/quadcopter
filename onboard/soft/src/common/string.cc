98515bb91ca5f98566e62b16611e86476279379b
javol <javol@timelinex.(none)>
2012-10-20 02:42:28 +0400

    b32tohex fixed again

--- onboard/soft/src/common/string.cc
+++ onboard/soft/src/common/string.cc
@@ -12,16 +12,13 @@ unsigned int strlen(const char* str) {
 
 /* 32 bit binary -> 64 bit hex */
 ustring b32tohex(unsigned int x) {
-    union {
-	ustring ll;
-	unsigned char q[8];
-    };
+    ustring ll = 0;
     const char* hex = "0123456789ABCDEF";
-    for(int i = 0; i  < 8; ++i) {
-	q[i] = hex[x >> 28];
+    for(int i = 7; i >= 0; --i) {
+	ll |= ((ustring)hex[x >> 28]) << (8 * i);
 	x <<= 4;
     }
-    return ll;
+     return ll;
 }
 
 ustring b32todec(int y) {
