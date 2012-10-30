c8b3264c05ba6e0480967a73e6487cae2ee57d22
Dmitry Duka <enjoy.cows@gmail.com>
2012-10-30 16:59:16 +0400

    added config file to setup the system

--- onboard/soft/src/radio/console.h
+++ onboard/soft/src/radio/console.h
@@ -1,6 +1,8 @@
 #ifndef CONSOLE_H
 #define CONSOLE_H
 
+#if CONSOLE_ENABLED == 1
 void parseConsoleMessage(const char*);
+#endif
 
 #endif
--- onboard/soft/src/radio/messages.cc
+++ onboard/soft/src/radio/messages.cc
@@ -8,7 +8,11 @@ namespace Messages {
 void defaultHandler(char*) {}
 
 void consoleHandler(char* str) {
+#if CONSOLE_ENABLED == 1
     parseConsoleMessage(str);
+#else
+    System::Bus::UART::write_waiting(":|\n");
+#endif
 }
 
 }
