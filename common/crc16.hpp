2e145e7e04331520a7d090d3801097645d7f0ece
Dmitry Duka <enjoy.cows@gmail.com>
2012-12-01 02:37:30 +0400

    crc16 is in common now

---
+++ common/crc16.hpp
@@ -0,0 +1,17 @@
+#ifndef CRC16_HPP
+#define CRC16_HPP
+
+unsigned short crc16(unsigned char *pcBlock, unsigned short len)
+{
+    unsigned short crc = 0xFFFF;
+    unsigned char i;
+
+    while (len--) {
+        crc ^= *pcBlock++ << 8;
+        for (i = 0; i < 8; i++)
+            crc = crc & 0x8000 ? (crc << 1) ^ 0x1021 : crc << 1;
+    }
+    return crc;
+}
+
+#endif
