fbc06c27a0e99f0ca7777b955c43c524937843c3
Dmitry Duka <enjoy.cows@gmail.com>
2012-12-02 02:02:40 +0400

    bootclient changes: bootloader messages implementations; ugly code, but works, yet to make it look better

--- ground/bootclient/main.cc
+++ ground/bootclient/main.cc
@@ -11,6 +11,7 @@
 #include <boost/program_options.hpp>
 #include <boost/asio.hpp>
 
+#include "crc16.hpp"
 #include "serial.hpp"
 #include "radio/message_types.h"
 
@@ -23,7 +24,7 @@ using std::endl;
 
 namespace settings {
     std::string input;
-    std::string serial_device = "/dev/ttyUSB0";
+    std::string serial_device = "/dev/ttyACM0";
     unsigned int speed = 115200;
     unsigned int tcp_port = 3009;
 };
@@ -62,8 +63,110 @@ void print_section(const std::vector<unsigned char>& c) {
     cout << std::dec << std::setw(1);
 }
 
-void output_section(const std::vector<unsigned char>& c) {
-}
+struct bootloader_client {
+    boost::shared_ptr<io> output;
+    size_t max_packet_size;
+    char * code_starts;
+
+    bootloader_client(boost::shared_ptr<io> o) : output(o) {
+        if(!ping()) throw std::runtime_error("Bootloader doesn't respond");
+        cout << "Bootloader responded." << endl;
+        max_packet_size = get_max_packet_size();
+        get_code_starts();
+    }
+
+    bool ping() {
+        output->write('?');
+        unsigned char ch;
+        if(output->read(ch))
+            if(ch == '!') return true;
+        return false;
+    }
+
+    size_t get_max_packet_size() { return 32; }
+
+    void set_address(char * a) {
+        output->write('%');
+        unsigned char data[6];
+        unsigned int x = reinterpret_cast<unsigned int>(a);
+        data[0] = x & 0xFF;
+        data[1] = (x >> 8) & 0xFF;
+        data[2] = (x >> 16) & 0xFF;
+        data[3] = (x >> 24) & 0xFF;
+        unsigned short int crc = crc16(data, 4);
+        data[4] = crc & 0xFF;
+        data[5] = crc >> 8;
+        for(int i = 0; i < 6; ++i)
+            output->write(data[i]);
+        unsigned char ch;
+        if(!output->read(ch))
+            throw std::runtime_error("Error reading response after setting the start address");
+        if(ch != '%')
+            throw std::runtime_error("Bootloader response is invalid (start address)");
+        if(!output->read(ch))
+            throw std::runtime_error("Error reading response after setting the start address");
+        if(ch != '!')
+            throw std::runtime_error("Bootloader responded the start address is invalid");
+        cout << "Set address to " << "0x" << std::setw(8) << std::setfill('0') << std::hex << x << std::dec << endl;
+    }
+
+    void output_section(const std::vector<unsigned char>& c, char* address) {
+        set_address(address);
+        size_t chunks = c.size() / max_packet_size;
+        size_t left = c.size() - chunks * max_packet_size;
+        std::vector<unsigned char> data = {0, static_cast<unsigned char>(max_packet_size & 0xFF),
+                                              static_cast<unsigned char>((max_packet_size >> 8) & 0xFF) };
+        for(int i = 0; i < chunks; ++i) {
+            data.resize(3);
+            for(int k = 0; k < max_packet_size; ++k)
+                data.push_back(c[max_packet_size * i + k]);
+            unsigned short int crc = crc16(&data[0], data.size());
+            data.push_back(crc & 0xFF);
+            data.push_back(crc >> 8);
+            for(int k = 0; k < data.size(); ++k)
+                output->write(data[k]);
+            /* Entire message is written, read response now */
+            unsigned char ch;
+            if(!output->read(ch))
+                throw std::runtime_error("Error reading response after the write message");
+            if(ch != 0 /*Write*/)
+                throw std::runtime_error("Bootloader response after the write message is invalid");
+            unsigned char d[2];
+            for(int k = 0; k < 2; ++k)
+                if(!output->read(d[k]))
+                    throw std::runtime_error("Error reading response after the write message");
+            size_t writtenBytes = d[0] | (d[1] << 8);
+            crc = crc16(d, 2);
+            for(int k = 0; k < 2; ++k)
+                if(!output->read(d[k]))
+                    throw std::runtime_error("Error reading response after the write message");
+            unsigned short crc_b = d[0] | (d[1] << 8);
+            if(crc != crc_b)
+                throw std::runtime_error("Error reading response after the write message: CRC is wrong");
+        }
+    }
+
+    char* get_code_starts() {
+        output->write('^');
+        unsigned char data[6];
+        unsigned char ch;
+        if(output->read(ch)) {
+            if(ch == '^') {
+                /* Read address and crc */
+                for(int i = 0; i < 6; ++i)
+                    if(!output->read(data[i]))
+                        throw std::runtime_error("Error while trying to read CODE_STARTS symbol from the bootloader");
+                /* Check crc */
+                if(crc16(data, 4) != (data[4] | data[5] << 8))
+                    throw std::runtime_error("Error while trying to read CODE_STARTS symbol from the bootloader");
+                unsigned int cs = data[0] | (data[1] << 8) | (data[2] << 16) | (data[3] << 24);
+                code_starts = reinterpret_cast<char*>(cs);
+                cout << "Code starts at " << "0x" << std::setw(8) << std::setfill('0') << std::hex << cs << std::dec << endl;
+            }
+        }
+    }
+
+};
 
 int main(int argc, char** argv) {
     // Declare the supported options.
@@ -109,22 +212,22 @@ int main(int argc, char** argv) {
         if(!vm.count("serial") && !vm.count("host"))
             throw std::runtime_error("You should specify the means of output - either serial device or TCP host/port");
 
-        boost::asio::io_service io;
-        boost::shared_ptr<io> output(new serial_port(io, settings::speed, settings::serial_device));
+        boost::asio::io_service ioserv;
+        boost::shared_ptr<io> output(new serial_port(ioserv, settings::speed, settings::serial_device));
+        bootloader_client bc(output);
 
-        bfd_holder abfd(settings::input);
+        /*bfd_holder abfd(settings::input);
         std::vector<unsigned char> init = abfd.get_section(".init");
         std::vector<unsigned char> boot = abfd.get_section(".boot");
         std::vector<unsigned char> tramp = abfd.get_section(".tramp");
         std::vector<unsigned char> text = abfd.get_section(".text");
-        std::vector<unsigned char> data = abfd.get_section(".data");
+        std::vector<unsigned char> data = abfd.get_section(".data");*/
         /* Open serial port */
-
-        
     } catch (std::exception& e) {
         /* Errors */
         std::cerr << e.what() << std::endl;
     }
 
+    cout << "Finishing" << endl;
     return 0;
 }
